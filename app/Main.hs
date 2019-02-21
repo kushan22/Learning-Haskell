fc8829df7edcab8323a45b60f4499ffbe6ce6366
Kushan Singh <kushansingh22@gmail.com>
2019-02-21 14:06:24 -0500

    Done with Haskell Assignment

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,55 @@
 module Main where
 
 import Lib
+import Data.List
 
-newOr a b = if a then a else b
+
+--Reverse a list
+reverseList :: [a] -> [a]
+reverseList [] = []
+reverseList (x:xs) = reverse xs ++ [x]
+
+--Union of Two Lists
+unionList :: (Eq a) => [a] -> [a] -> [a]
+unionList [] [a] = [a]
+unionList [a] [] = [a]
+
+unionList xs ys = removeDuplicates(xs ++ ys)
+removeDuplicates [] = []
+removeDuplicates (x:xs) = if x `elem` xs then removeDuplicates xs
+                          else x : removeDuplicates xs      
+
+--Sorting a list
+sortList :: Ord a => [a] -> [a]
+sortList [] = []
+sortList (p:xs) = (sortList lesser) ++ [p] ++ (sortList greater)
+        where
+          lesser  = filter (< p) xs
+          greater = filter (>= p) xs
+
+--Maximum Element
+maxNum :: (Ord a) => [a] -> a
+maxNum [] = error "List is Empty"
+maxNum [x] = x
+maxNum (x:xs) | (maxNum xs) > x = maxNum xs
+               | otherwise  = x
+
+--Minimum Element               
+minNum :: (Ord a) => [a] -> a
+minNum [] = error "List is Empty"
+minNum [x] = x
+minNum (x:xs) | (minNum xs) < x = minNum xs
+              | otherwise = x  
+
+--Permutation of a List
+permutation :: Eq a => [a] -> [[a]]
+permutation [] = [[]]
+permutation xs = [x:ys | x <- xs, ys <- permutation (delete x xs)]
+                   
+               
 
 main :: IO ()
 main = do
-        let numbers = [1,2,3,4]
-        let second = [5,6,7]
-        let concatenated = numbers ++ second
-        print (concatenated)
+        print (minNum [1,2,3])
     
 
