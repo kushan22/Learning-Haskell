b450c6c1fa5ec70c2c5aab5bf19b2ead61ed90c5
Kushan Singh <kushansingh22@gmail.com>
2019-02-20 19:40:24 -0500

    Getting there

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,13 @@ module Main where
 
 import Lib
 
+newOr a b = if a then a else b
+
 main :: IO ()
-main = putStrLn "Hey Wassup Haskell"
+main = do
+        let numbers = [1,2,3,4]
+        let second = [5,6,7]
+        let concatenated = numbers ++ second
+        print (concatenated)
+    
+
